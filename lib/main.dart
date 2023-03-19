484cf01887f0a97907c9fe4353526a47b04c32e5
birjuvachhani <brvachhani@gmail.com>
2023-03-19 15:08:51 +0530

    Add links

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
 import 'cursor_blob.dart';
 
 void main() => runApp(const MyApp());
@@ -33,22 +36,68 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SizedBox.expand(
         child: CursorBlob(
-          child: Center(
-            child: Text(
-              'poppr',
-              style: TextStyle(
-                fontSize: 56,
-                letterSpacing: -4,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF16F1D1),
+          child: Stack(
+            children: [
+              Positioned(
+                top: 16,
+                right: 16,
+                child: TextButton(
+                  onPressed: () {
+                    launchUrlString(
+                        'https://github.com/BirjuVachhani/cursor_blob');
+                  },
+                  child: const Text('View on Github'),
+                ),
               ),
-            ),
-          )
+              const Center(
+                child: Text(
+                  'poppr',
+                  style: TextStyle(
+                    fontSize: 56,
+                    letterSpacing: -4,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF16F1D1),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 16,
+                left: 16,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text.rich(
+                      TextSpan(
+                        text: 'Inspired by ',
+                        children: [
+                          TextSpan(
+                            text: 'poppr.be',
+                            mouseCursor: SystemMouseCursors.click,
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                launchUrlString('https://poppr.be');
+                              },
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF16F1D1),
+                              decoration: TextDecoration.underline,
+                              decorationColor: Color(0xFF16F1D1),
+                            )
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
+      ),
     );
   }
 }
